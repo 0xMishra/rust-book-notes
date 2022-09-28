e65209997a27218e7588c2c2597c65ad87877a8f
mishra811 <mishrakundan073@gmail.com>
2022-09-28 13:13:48 +0530

    multi threaded server initialised

---
+++ rust-book/hello/src/lib.rs
@@ -0,0 +1,26 @@
+use std::thread::{self, JoinHandle};
+pub struct ThreadPool {
+    threads: Vec<thread::JoinHandle<()>>,
+}
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        /// Create a new ThreadPool.
+        ///
+        /// The size is the number of threads in the pool.
+        ///
+        /// # Panics
+        ///
+        /// The `new` function will panic if the size is zero.   
+        assert!(size > 0);
+        let mut threads = Vec::with_capacity(size);
+        for _ in 0..size {}
+        ThreadPool { threads }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+    }
+}
--- rust-book/hello/src/main.rs
+++ rust-book/hello/src/main.rs
@@ -1,25 +1,34 @@
+use hello::ThreadPool;
 use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let buff_reader = BufReader::new(&mut stream);
     let request_line = buff_reader.lines().next().unwrap().unwrap();
-    let (filename, status_line) = if request_line == "GET / HTTP/1.1" {
-        ("hello.html", "HTTP/1.1 200 OK")
-    } else {
-        ("404.html", "HTTP/1.1 404 NOT FOUND")
-    };
+    let (filename, status_line) = match &request_line[..] {
+        "GET /HTTP/1.1" => ("hello.html", "HTTP/1.1 200 OK"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
 
+            ("hello.html", "HTTP/1.1 200 OK")
+        }
+        _ => ("hello.html", "HTTP/1.1 200 OK"),
+    };
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
