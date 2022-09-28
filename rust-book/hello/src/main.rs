1f907b094b060a6e59e56674c70306fc1ce28c40
mishra811 <mishrakundan073@gmail.com>
2022-09-28 12:23:03 +0530

    single threaded server

--- rust-book/hello/src/main.rs
+++ rust-book/hello/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
@@ -12,10 +13,15 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buff_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buff_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-    println!("request: {:#?}", http_request);
+    let request_line = buff_reader.lines().next().unwrap().unwrap();
+    let (filename, status_line) = if request_line == "GET / HTTP/1.1" {
+        ("hello.html", "HTTP/1.1 200 OK")
+    } else {
+        ("404.html", "HTTP/1.1 404 NOT FOUND")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
 }
